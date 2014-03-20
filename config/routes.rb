ead017f93184ce59c9c6d73afe0355bf0d55080b
Eddie <eddie@Eddies-MacBook-Pro.local>
2014-03-20 14:09:04 -0600

    update 3/20

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 EdNewApp::Application.routes.draw do
   get  "/"          => "main#root"
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,4 +55,4 @@ EdNewApp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
